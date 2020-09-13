ef9ea100334cbc9f0fd460af08af7b8b519b655f
Yamilet Gutierrez <yzg0057@auburn.edu>
2020-09-13 13:37:11 -0700

    created new user control for tabs functionality

---
+++ WebBrowser.UI/UserControlTabs.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WebBrowser.UI
+{
+    partial class UserControlTabs
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserControlTabs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControlTabs";
+            this.Size = new System.Drawing.Size(1210, 708);
+            this.Load += new System.EventHandler(this.UserControlTabs_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
