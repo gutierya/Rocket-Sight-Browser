932303b38b0b8ce311d9d811edc76f1adeeff0cb
Yamilet Gutierrez <yzg0057@auburn.edu>
2020-09-20 19:13:03 -0700

    Added history item and bookmark item classes w/properties

---
+++ WebBrowser.logic/BookmarkItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser.logic
+{
+    class BookmarkItem
+    {
+        public int Id { get; set; }
+        public string URL { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ WebBrowser.logic/HistoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser.logic
+{
+    class HistoryItem
+    {
+        public int Id { get; set }
+        public string URL { get; set }
+        public string Title { get; set }
+        public DateTime Date { get; set }
+    }
+}
