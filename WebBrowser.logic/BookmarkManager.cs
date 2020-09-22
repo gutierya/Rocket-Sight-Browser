0a2a3cd4be5046663b10af0f781ed4b7f29e8c6f
Yamilet Gutierrez <yzg0057@auburn.edu>
2020-09-21 19:04:19 -0700

    Fixed references in logic and ui files, fixed logic in tab controls and created objects to reference in ui

--- WebBrowser.logic/BookmarkManager.cs
+++ WebBrowser.logic/BookmarkManager.cs
@@ -3,34 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebBrowser.Data.HistoryBookmarkDBDataSetTableAdapters;
 
 namespace WebBrowser.logic
 {
-    public class BookmarkManager
+    class BookmarkManager
     {
-        public static void AddItem(BookmarkItem item)
-        {
-            var adapter = new BookmarksTableAdapter();
-            adapter.Insert(item.URL, item.Title);
-        }
-
-        public static List<BookmarkItem> GetItems()
-        {
-            var adapter = new BookmarksTableAdapter();
-            var results = new List<BookmarkItem>();
-            var rows = adapter.GetData();
-
-            foreach (var row in rows)
-            {
-                var item = new BookmarkItem();
-                item.URL = row.URL;
-                item.Title = row.Title;
-                item.Id = row.Id;
-
-                results.Add(item);
-            }
-            return results;
-        }
     }
 }
