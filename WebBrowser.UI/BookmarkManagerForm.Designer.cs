4d971c2ad4c67bf9697d78519c35bc55dffb355a
Yamilet Gutierrez <yzg0057@auburn.edu>
2020-09-27 17:34:06 -0700

    Added search term textbox and search button functionality. Added delete and clear functionalty

--- WebBrowser.UI/BookmarkManagerForm.Designer.cs
+++ WebBrowser.UI/BookmarkManagerForm.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxSearchResults = new System.Windows.Forms.TextBox();
+            this.resultsLabelforList = new System.Windows.Forms.Label();
+            this.insertLabelforList = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -40,21 +46,89 @@
             this.listBox1.Size = new System.Drawing.Size(468, 479);
             this.listBox1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(845, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search Bookmark:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(845, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear Bookmark:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(845, 514);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete Bookmark:";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxSearchResults
+            // 
+            this.textBoxSearchResults.Location = new System.Drawing.Point(741, 138);
+            this.textBoxSearchResults.Name = "textBoxSearchResults";
+            this.textBoxSearchResults.Size = new System.Drawing.Size(400, 31);
+            this.textBoxSearchResults.TabIndex = 4;
+            // 
+            // resultsLabelforList
+            // 
+            this.resultsLabelforList.AutoSize = true;
+            this.resultsLabelforList.Location = new System.Drawing.Point(265, 66);
+            this.resultsLabelforList.Name = "resultsLabelforList";
+            this.resultsLabelforList.Size = new System.Drawing.Size(146, 25);
+            this.resultsLabelforList.TabIndex = 5;
+            this.resultsLabelforList.Text = "Output Below:";
+            // 
+            // insertLabelforList
+            // 
+            this.insertLabelforList.AutoSize = true;
+            this.insertLabelforList.Location = new System.Drawing.Point(840, 66);
+            this.insertLabelforList.Name = "insertLabelforList";
+            this.insertLabelforList.Size = new System.Drawing.Size(248, 25);
+            this.insertLabelforList.TabIndex = 6;
+            this.insertLabelforList.Text = "Enter a Search text item:";
+            // 
             // BookmarkManagerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1352, 866);
+            this.ClientSize = new System.Drawing.Size(1300, 712);
+            this.Controls.Add(this.insertLabelforList);
+            this.Controls.Add(this.resultsLabelforList);
+            this.Controls.Add(this.textBoxSearchResults);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "BookmarkManagerForm";
             this.Text = "BookmarkManagerForm";
             this.Load += new System.EventHandler(this.BookmarkManagerForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxSearchResults;
+        private System.Windows.Forms.Label resultsLabelforList;
+        private System.Windows.Forms.Label insertLabelforList;
     }
 }
--- WebBrowser.UI/BookmarkManagerForm.cs
+++ WebBrowser.UI/BookmarkManagerForm.cs
@@ -23,11 +23,42 @@ namespace WebBrowser.UI
             //get all bookmark items
             var items = BookmarkManager.GetBookmarkItems();
             listBox1.Items.Clear(); //listbox of bookmark form
-
+            
             //Display in listbox
             foreach (var item in items)
             {
                 listBox1.Items.Add(string.Format("{0} ({1})", item.Title, item.URL));
+            }
+        }
+
+        //search button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var items = HistoryManager.GetHistoryItems();
+
+            listBox1.Items.Clear();
+
+            foreach (var item in items)
+            {
+                if (item.Title.Contains(textBoxSearchResults.Text) || item.URL.Contains(textBoxSearchResults.Text))
+                {
+                    listBox1.Items.Add(String.Format("[{0}] {1} ({2})", item.Date, item.Title, item.URL));
+                }  
+            }
+        }
+
+        //clear results button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        //delete bookmark button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedIndex >= 0)
+            {
+                this.listBox1.Items.RemoveAt(this.listBox1.SelectedIndex);
             }
         }
     }
